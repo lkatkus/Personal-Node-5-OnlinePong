0a1527e4495c62b6e5f43a0a440d30e1b603e9b8
Laimonas Katkus <laimonas@katkus.eu>
2018-06-02 18:16:39 +0300

    added player controls, ball and basic animation, setup serverside gamestate

--- public/js/game.js
+++ public/js/game.js
@@ -1,7 +1,13 @@
 // Selectors
+const container = document.querySelector(".container");
 const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = container.offsetWidth;
+canvas.height = container.offsetHeight;
 
-let player;
+let currentGameState;
+let user;
+let ball;
 const socket = io();
 
 class User{
@@ -9,36 +15,124 @@ class User{
         this.socket = socket;
         this.token = token;
         this.status = status;
+        
+        this.playerWidth = 20;
+        this.playerHeight = canvas.height / 10;
+        this.playerPosition = 0;
+        
+        if(status === 'player1'){
+            this.playerColor = 'red';
+        }else if(status === 'player2'){
+            this.playerColor = 'green';
+        }
+
+        this.playerImage = this.createPlayerImage();
     }
 
-    click(event){
-        console.log('clicking', this.status);
-        socket.emit('clicked', {socket: socket.id, token: this.token, y: event.offsetY}, (err) => {
-            console.log(err);
-        });
-    }
-
+    // Add all needed event listeners
     addEventListeners(){
-        canvas.addEventListener('click', (event) => {
-            player.click(event);
-        });
+        canvas.addEventListener('mousemove', (event) => {
+            
+            // Converts player position px to percent
+            let playerY = (event.offsetY * 100) / canvas.height;
+
+            socket.emit('playerMoving', {
+                socket: socket.id,
+                status: this.status,
+                newPosition: playerY
+            });
+        })
+    }
+
+    // Creates player image / paddle
+    createPlayerImage(){
+        ctx.fillStyle = this.playerColor;
+        ctx.fillRect(0, 0, this.playerWidth, this.playerHeight);
+        let playerImage = ctx.getImageData(0, 0, this.playerWidth, this.playerHeight);
+        void ctx.clearRect(0, 0, canvas.width, canvas.height);
+        return playerImage;
     }
 }
 
+class Ball{
+    constructor(){
+        // Position
+        this.posX = canvas.width / 2;
+        this.posY = canvas.height / 2;
+        // Size
+        this.radius = 10;
+        this.color = 'blue';
+        // Movement
+        this.speedX = 10;
+        this.speedY = 10;
+    }
+
+    drawBall(){
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(convertWidth(currentGameState.ball.x), 200, this.radius, 0, Math.PI*2, true);
+        ctx.fill();
+    }
+}
+
+// Socket event listeners
+// Main listener for creating new player on connection
 socket.on('newPlayer', (data, callback) => {
-    console.log('new player', data);
-    player = new User(socket, data.token, data.status);
+    user = new User(socket, data.token, data.status);
     
-    if(data.status === 'player'){
-        player.addEventListeners();
+
+    if(data.status === 'player1' || data.status === 'player2'){
+        user.addEventListeners();
     }
 
     return callback({socket: socket.id, token: data.token});
 });
 
+// Listener for updating player positions
+socket.on('playerMoved', (gameState) => {
+    // Update game state with new player locations
+    currentGameState = gameState;
+});
+
+socket.on('ballMoved', (gameState) => {
+    console.log('ballMoved')
+    currentGameState = gameState;
+})
+
 socket.on('newViewer', () => {
     console.log('viewing');
 })
 
+socket.on('startGame', (gameState) => {
+    console.log('========= STARTING GAME =========');
+    console.log(`========= ${user.status} =========`);
 
+    // Sets currentGameState to default
+    currentGameState = gameState;
+    
+    // Creates a ball instance
+    ball = new Ball();
 
+    // Starts animation
+    setInterval(() => {
+        animation();
+    }, 1000 / 30)
+})
+
+const animation = () => {
+    window.requestAnimationFrame(() => {
+              
+        void ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.putImageData(user.playerImage, 50, convertHeight(currentGameState.players.player1.position) - user.playerHeight / 2);
+        ctx.putImageData(user.playerImage, canvas.width - 50 - user.playerWidth, convertHeight(currentGameState.players.player2.position) - user.playerHeight / 2);
+        ball.drawBall();
+    });
+}
+
+const convertWidth = (percent) => {
+    return (canvas.width * percent) / 100;
+}
+
+const convertHeight = (percent) => {
+    return (canvas.height * percent) / 100;
+}
--- server/server.js
+++ server/server.js
@@ -16,31 +16,85 @@ const io = socketIO(server);
 // MIddleware
 app.use(express.static(publicPath));
 
-const playerArray = [];
+let playerArray = [];
+let gameInterval;
+
+const gameState = {
+    ball: {
+        x: 50,
+        y: 50
+    },
+    players: {
+        player1: {
+            position: 50
+        },
+        player2: {
+            position: 50
+        }
+    }
+}
 
 // Routes and listener
 io.on('connection', (socket) => {
     
+    console.log('connection');
+
     // Check if game is not full
     if(playerArray.length < 2){
-        socket.emit('newPlayer', {token:'token1', status: 'player'}, (player) => {
+        
+        let status;
+
+        if(playerArray.length === 0){ /* !!! Needs fixing. If player1 leaves, then length != 0 and other player gets player2. Fix - add checker which player is present !!! */
+            console.log('player 1');
+            status = 'player1';
+        }else{
+            console.log('player 2');
+            status = 'player2';
+        }      
+
+        socket.emit('newPlayer', {token:'token1', status}, (player) => {
+            // Adds returned player object to players array
             playerArray.push(player);
-            console.log('adding', playerArray);
+            
+            // Start game if 2 players are present
+            if(playerArray.length === 2){
+                io.emit('startGame', gameState);
+                let speed = 5;
+
+                gameInterval = setInterval(() => {
+                    gameState.ball.x += speed;
+                    if(gameState.ball.x >= 100){
+                        speed = -speed;
+                    }else if(gameState.ball.x <= 0){
+                        speed = -speed;
+                    }
+                    io.emit('ballMoved', gameState);
+                }, 1000 / 2)
+            };
         });
     }else{
-        socket.emit('newPlayer', {token:'token1', status: 'viewer'}, (player) => {
-            console.log('adding', playerArray);
-        });
+        // Set user status to 'viewer' if already 2 players present
+        socket.emit('newPlayer', {token:'token1', status: 'viewer'});
     }
 
-    // Send game token to player
+    socket.on('playerMoving', (player) => {
+        // Update gameState with new player position
+        gameState.players[player.status].position = player.newPosition;
 
-    socket.on('clicked', (data) => {
-        console.log(`Player ${data.socket} clicked`);
+        io.emit('playerMoved', gameState);
+    })
+
+    socket.on('ballMoved', (newBallPosition) => {
+        console.log(newBallPosition);
     });
 
     socket.on('disconnect', () => {
-        console.log('disconnected')
+        // Remove disconnected player and free a spot for new player
+        playerArray = playerArray.filter((item) => {
+            return item.socket !== socket.id;
+        })
+
+        clearInterval(gameInterval);
     })
 })
 
